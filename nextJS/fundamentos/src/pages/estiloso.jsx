a575beb8fbc6b3e790be8455738a789c9749b95c
mirandaeverton <mir.everton@gmail.com>
2022-04-15 12:31:36 +0100

    CSS Module class

---
+++ nextJS/fundamentos/src/pages/estiloso.jsx
@@ -0,0 +1,6 @@
+import styles from '../styles/Estiloso.module.css'
+export default function Estiloso() {
+    return <div className={styles.roxo}>
+        <h1>Estilo usando CSS Modules</h1>
+    </div>
+}
---
+++ nextJS/fundamentos/src/pages/index.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div>
+            <h1>Fundamento de NextJS & React</h1>
+            <h2>Simbora pessoal!!!</h2>
+        </div>
+    )
+}

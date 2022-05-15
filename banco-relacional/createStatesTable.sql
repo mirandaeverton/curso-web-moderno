8f540a05fac31e0bb3c1c5e125b1b5a89977f381
mirandaeverton <mir.everton@gmail.com>
2022-05-15 11:28:56 +0100

    Create first table

---
+++ banco-relacional/createStatesTable.sql
@@ -0,0 +1,12 @@
+-- Creating a BR States table
+
+create table states (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(45) NOT NULL,
+    sigla VARCHAR(2) NOT NULL,
+    regiao ENUM('Norte', 'Nordeste', 'Centro-Oeste', 'Sudeste', 'Sul') NOT NULL,
+    populacao DECIMAL(5,2) NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY (nome),
+    UNIQUE KEY (sigla)
+);
